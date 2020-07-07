1cd2f9c2ed37b8feb5171dbd555e7de3d5f4fc9c
jiangchuan <jiangchuan@ifeng.com>
2020-07-07 16:46:57 +0800

    add

--- src/main/java/springboot/Application.java
+++ src/main/java/springboot/Application.java
@@ -2,11 +2,13 @@ package springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * Created by fengtingting on 2020/6/30.
  */
 @SpringBootApplication
+@ImportResource("classpath:applicationContext.xml")
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/springboot/GetController.java
@@ -0,0 +1,22 @@
+package springboot;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class GetController {
+
+
+    @RequestMapping(path = "/helloWorld", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloWorld(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        return "I'm jiagnchuan";
+    }
+
+}
