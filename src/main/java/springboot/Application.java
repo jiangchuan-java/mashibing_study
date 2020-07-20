2b9030d76fbaacd3e62858162e134e73110876f4
jiangchuan <jiangchuan@ifeng.com>
2020-07-20 10:52:14 +0800

    add

--- src/main/java/springboot/Application.java
+++ src/main/java/springboot/Application.java
@@ -2,7 +2,9 @@ package springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by fengtingting on 2020/6/30.
@@ -12,7 +14,13 @@ import org.springframework.context.annotation.ImportResource;
 public class Application {
 
     public static void main(String[] args) {
+        //启动springboot
         SpringApplication springApplication = new SpringApplication(Application.class);
         springApplication.run(args);
+
+        //创建Spring上下文
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+        //获取bean的实例
+        Object object = context.getBean("myFirstSpringDemo");
     }
 }
