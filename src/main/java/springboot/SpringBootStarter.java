6f3dacc89e1a8b76b1875c0e92955aa70b77ddff
ftt <673973309@qq.com>
2020-07-26 12:05:27 +0800

    add

--- src/main/java/springboot/SpringBootStarter.java
+++ src/main/java/springboot/SpringBootStarter.java
@@ -9,26 +9,30 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.HashMap;
-
 /**
  * Created by fengtingting on 2020/6/30.
  */
 @SpringBootApplication
 @ImportResource("classpath:applicationContext.xml")
-public class Application {
+public class SpringBootStarter {
 
-    public static void main(String[] args) {
-        //启动springboot
-        SpringApplication springApplication = new SpringApplication(Application.class);
-        ConfigurableApplicationContext applicationContext = springApplication.run(args);
-        BeanDefinition beanDefinition = applicationContext.getBeanFactory().getBeanDefinition("student");
-
+    private static void buildSpring(String[] args){
         /*创建这种XmlContext时， beanFactory是在refresh方法里的ObtainBeanFactory中refreshBeanFactory时进行创建的*/
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         /*创建这种AnotationContext时，在对应的构造方法里创建了beanFactoryr*/
         context = new AnnotationConfigApplicationContext();
         //获取bean的实例
         Object object = context.getBean("myFirstSpringDemo");
+    }
+
+    private static void buldSpringBoot(String[] args){
+        //启动springboot
+        SpringApplication springApplication = new SpringApplication(SpringBootStarter.class);
+        ConfigurableApplicationContext applicationContext = springApplication.run(args);
+        BeanDefinition beanDefinition = applicationContext.getBeanFactory().getBeanDefinition("student");
+    }
+
+    public static void main(String[] args) {
+        buldSpringBoot(args);
     }
 }
---
+++ src/main/java/springboot/ThreadPoolExecutorConfiguration.java
@@ -0,0 +1,35 @@
+package springboot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.*;
+
+@Configuration
+/**
+ * @Configuration注解与<Beans></Beans>标签效果等同。
+ * 标识这个类中含有一个或多个@Bean声明的实体
+ */
+public class ThreadPoolExecutorConfiguration {
+
+    @Bean
+    public ThreadPoolExecutor getSingleThreadPoolExecutor(){
+        ThreadPoolExecutor singlePoolExecutor =
+                new ThreadPoolExecutor(1,1,60, TimeUnit.SECONDS,new LinkedBlockingQueue<Runnable>());
+        return singlePoolExecutor;
+    }
+
+    @Bean
+    public ThreadPoolExecutor getCacheThreadPoolExecutor(){
+        ThreadPoolExecutor cachePoolExecutor =
+                new ThreadPoolExecutor(1,Integer.MAX_VALUE,60, TimeUnit.SECONDS,new LinkedBlockingQueue<Runnable>());
+        return cachePoolExecutor;
+    }
+
+    @Bean
+    public ThreadPoolExecutor getFixedThreadPoolExecutor(){
+        ThreadPoolExecutor fixedPoolExecutor =
+                new ThreadPoolExecutor(200,200,60, TimeUnit.SECONDS,new SynchronousQueue<>());
+        return fixedPoolExecutor;
+    }
+}
