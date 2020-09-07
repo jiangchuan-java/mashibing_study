69dab6fdfd4b46ae20d54eb5c0ffe2b40a738372
jiangchuan <jiangchuan@ifeng.com>
2020-09-07 20:13:40 +0800

    add

--- src/main/java/java_/singlton/Final_Singlton.java
+++ src/main/java/java_/singlton/Final_Singlton.java
@@ -11,7 +11,11 @@ public class Final_Singlton {
     private final int flag;
 
     private Final_Singlton(){
-        flag = 1; //final 关键字能保证，在对象的引用被其他人看到时，final属性一定被初始化，进而防止了创建对象的重排序
+        flag = 1;
+        /**
+         * final 关键字能保证，在对象的引用被其他人看到时，
+         * final属性一定被初始化，进而防止了创建对象的重排序
+         */
     }
     private static Final_Singlton instance;
 
