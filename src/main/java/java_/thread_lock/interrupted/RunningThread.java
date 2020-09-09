c77b1659e1b24c6151ab4df9c37e39340a8009e9
jiangchuan <jiangchuan@ifeng.com>
2020-09-09 20:10:10 +0800

    add

---
+++ src/main/java/java_/thread_lock/interrupted/RunningThread.java
@@ -0,0 +1,32 @@
+package java_.thread_lock.interrupted;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Des:
+ * @Author: jiangchuan
+ * <p>
+ * @Date: 20-9-9
+ */
+public class RunningThread extends Thread{
+
+    @Override
+    public void run(){
+        while (true) {
+            if(Thread.interrupted()){
+                System.out.println("receive interrupted");
+                break;
+            } else {
+                System.out.println("run");
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        RunningThread runningThread = new RunningThread();
+        runningThread.start();
+        TimeUnit.SECONDS.sleep(2);
+        runningThread.interrupt();
+    }
+
+}
