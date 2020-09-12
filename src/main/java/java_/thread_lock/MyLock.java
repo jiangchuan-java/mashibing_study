159ce455ae6a811bd5515987a63a86f019d8a608
ftt <673973309@qq.com>
2020-09-12 10:16:42 +0800

    add

--- src/main/java/java_/thread_lock/MyLock.java
+++ src/main/java/java_/thread_lock/MyLock.java
@@ -1,44 +1,49 @@
 package java_.thread_lock;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
 /**
  * Created by fengtingting on 2020/6/29.
  */
 public class MyLock {
-    private Sync sync;
 
-    public MyLock(){
-        sync = new Sync();
+   List<Thread> blockingList = new ArrayList<>();
+
+   private AtomicInteger state = new AtomicInteger(1);
+
+   public boolean tryLock(){
+        if(state.compareAndSet(1,0)){
+            //拿到锁了
+            return true;
+        } else {
+            //进入阻塞队列
+            Thread thread = Thread.currentThread();
+            return false;
+        }
     }
 
     public void lock(){
-        sync.acquire(1);
-    }
-
-    public void unlock(){
-        sync.release(1);
-    }
-
-    private static class Sync extends AbstractQueuedSynchronizer{
-        private volatile int state = 0;
-
-        protected  boolean tryAcquire(int acquires) {
-            int c = getState();
-            if (c == 0) {
-                return compareAndSetState(0, acquires);
+        while (true){
+            if(state.compareAndSet(1,0)){
+                break;
             }
-            return false;
         }
-
-        protected  boolean tryRelease(int releases) {
-            int c = getState() - releases;
-            boolean free = false;
-            if (c == 0) {
-                free = true;
+        if(state.compareAndSet(1,0)){
+            //拿到锁了
+        } else {
+            //进入阻塞队列
+            Thread thread = Thread.currentThread();
+            blockingList.add(thread);
+            while (true){
+                if(blockingList.get(0) == thread){ //队列中第一个
+                    if(state.compareAndSet(1,0)){
+                        break;
+                    }
+                }
             }
-            setState(c);
-            return free;
         }
     }
 }
