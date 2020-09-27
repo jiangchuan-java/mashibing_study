e56a26bfe9bcbdb0c27386f963640dc11febb57d
jiangchuan <jiangchuan@ifeng.com>
2020-09-27 15:35:59 +0800

    add

--- src/main/java/springboot/aop/ForAOPTest.java
+++ src/main/java/springboot/aop/ForAOPTest.java
@@ -1,7 +1,7 @@
 package springboot.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.*;
 
 /**}
  *
@@ -16,9 +16,9 @@ import org.aspectj.lang.annotation.Around;
 /**
  *
  * Aspect : 切面:常规类实现的
- * Join point: 连接点:在spring aop中，连接点始终代表方法的执行
- * Advide : 建议:切面在特定连接点处采取的操作，不同类型的建议包括"包围","前","后"，包括spring在内的许多aop框架都将建议建模为Interceptor，并在连接点周围维护一系列Interceptor
- * Pointcut : 切入点:表达式，通过表达式匹配出来的叫做连接点
+ * Join point: 连接点-某个方法的执行: 在spring aop中，连接点始终代表方法的执行
+ * Advide : 建议-特殊的操作: 切面在特定连接点处采取的操作，不同类型的建议包括"包围","前","后"，包括spring在内的许多aop框架都将建议建模为Interceptor，并在连接点周围维护一系列Interceptor
+ * Pointcut : 切入点-表达式: 通过表达式匹配出来的Join point，spring aop核心概念，可以应用在多个对象上
  * Introduction : 其他接口:spring可以用过Introduction使代理的对象实现新的接口
  * Target object : 目标对象:在spring aop中始终是代理对象, 这里应该是被代理的对象
  * AOP proxy : aop代理:在spring中，aop代理是JDK动态代理或CGLIB代理
@@ -28,7 +28,11 @@ import org.aspectj.lang.annotation.Around;
   */
 public class /*Aspect*/ ForAOPTest {
 
-    @Around/*advice*/("execution(* package.class.method(param))"/*pointcut*/)
+    @Around/*advice*/("execution(* package.class.method(param))"/*pointcut, */)
+    @Before("")
+    @After("")
+    @AfterReturning
+    @AfterThrowing
     public Object around(ProceedingJoinPoint/*join point*/ pjp) throws Throwable {
         System.out.println("123");
         return pjp.proceed();
