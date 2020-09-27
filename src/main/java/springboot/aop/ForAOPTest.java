2be6e5fad4c2e21f908d62fc79c7585f7040ee08
jiangchuan <jiangchuan@ifeng.com>
2020-09-27 16:14:08 +0800

    add

--- src/main/java/springboot/aop/ForAOPTest.java
+++ src/main/java/springboot/aop/ForAOPTest.java
@@ -2,6 +2,7 @@ package springboot.aop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
 
 /**}
  *
@@ -11,7 +12,7 @@ import org.aspectj.lang.annotation.*;
  * @Date: 20-8-6
  */
 //@Aspect
-//@Component
+//
 
 /**
  *
@@ -24,8 +25,15 @@ import org.aspectj.lang.annotation.*;
  * AOP proxy : aop代理:在spring中，aop代理是JDK动态代理或CGLIB代理
  * Weaving : 编织、织入：将切面与其他类链接在一起创建一个建议对象，这一过程可以在编译(Aspect编译器)，或加载，或运营时完成，spring通过运行时完成。
  *
+ * 注意事项：
+ * 1: spring aop 是纯java实现的，不需要特殊的编译过程，不需要控制类加载器的层次结构，因此适用性更广
+ * 2: spring aop 当前仅支持方法的连接点(建议在Spring Bean上执行方法),尽管可以在不破坏核心Spring AOP API的情况下添加对字段拦截的支持，但并未实现字段拦截。
+ *  如果需要建议字段访问和更新连接点，请考虑使用诸如AspectJ之类的语言。
+ * 3:
  *
   */
+@Aspect
+//@Component, 或者通过typeFilter 将Aspect注释的类可以在component-scan 扫描并加载到beanFacotory中
 public class /*Aspect*/ ForAOPTest {
 
     /*advice*/@Around(/*pointcut*/"execution(* package.class.method(param))")
@@ -37,4 +45,6 @@ public class /*Aspect*/ ForAOPTest {
         System.out.println("123");
         return pjp.proceed();
     }
+
+    /*@Pointcut("execution、within、this、target、args、@target、@args、@within、@annotation")*/
 }
