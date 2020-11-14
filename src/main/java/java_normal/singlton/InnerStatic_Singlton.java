9b95f2f8c2410e49818306af7091e1dd3c514163
ftt <673973309@qq.com>
2020-11-14 14:01:26 +0800

    add

---
+++ src/main/java/java_normal/singlton/InnerStatic_Singlton.java
@@ -0,0 +1,22 @@
+package java_normal.singlton;
+
+/**
+ * TODO
+ *
+ * @author fengtingting
+ * @version 1.0
+ * @date 2020/11/14 13:59
+ */
+public class InnerStatic_Singlton {
+
+    private InnerStatic_Singlton(){}
+
+
+    private static class InstanceHoldr {
+        private static InnerStatic_Singlton instance = new InnerStatic_Singlton();
+    }
+
+    public static InnerStatic_Singlton getInstance() {
+        return InstanceHoldr.instance;
+    }
+}
