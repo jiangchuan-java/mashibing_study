f9f315a2c6b4572b98e0dfdeab4f16b137b8eed2
jiangchuan <jiangchuan@ifeng.com>
2020-12-07 18:35:29 +0800

    add

---
+++ src/main/java/jvm/class_file/Father.java
@@ -0,0 +1,17 @@
+package jvm.class_file;
+
+/**
+ * @Author: jiangchuan
+ * <p>
+ * @Date: 20-12-7
+ */
+public class Father {
+
+    public static void f1() {
+        System.out.println("Father static f1() ");
+    }
+
+    public void f2() {
+        System.out.println("Father object f2() ");
+    }
+}
---
+++ src/main/java/jvm/class_file/Son.java
@@ -0,0 +1,14 @@
+package jvm.class_file;
+
+/**
+ * @Author: jiangchuan
+ * <p>
+ * @Date: 20-12-7
+ */
+public class Son extends Father{
+
+    @Override
+    public void f2() {
+        System.out.println("son object f2() ");
+    }
+}
